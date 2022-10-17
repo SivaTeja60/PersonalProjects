9a97a08aa5d84c12d700ad388da1de4bb4052dea
SivaTeja60 <sivatejakukkannagari@gmail.com>
2022-10-16 21:04:09 -0700

    Initial Commit

---
+++ madlibs/src.py
@@ -0,0 +1,10 @@
+#youtuber='shiva teja'
+##print("subscribe to "+youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+adj=input("adjective:")
+verb1=input("verb:")
+position=input("position:")
+
+madlib=f"Computer Programming is so {adj}! It makes me so excited all the time beacause I love to {verb1}. Dont give up and keep going to become the {position} you want"
+print(madlib)
