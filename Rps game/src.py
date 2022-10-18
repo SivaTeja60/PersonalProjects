874a96561005155810e7bb65d429b04d2ba9f672
SivaTeja60 <sivatejakukkannagari@gmail.com>
2022-10-18 16:49:32 -0700

    rps game commit

---
+++ Rps game/src.py
@@ -0,0 +1,17 @@
+from operator import truediv
+import random
+def play():
+    user=input('r for rock, p for paper, s for scissors -->')
+    computer=random.choice(['r','p','s' ])
+
+    if user== computer:
+        return "Its a tie"
+    if is_win(user,computer):
+        return "You won"
+    return "You Lost"
+    
+def is_win(player,opponent):
+    if (player=='r' and opponent=='s') or (player=='s' and opponent=='p') or (player=='p' and opponent=='r'):
+        return True
+
+print(play())
